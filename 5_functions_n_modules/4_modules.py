bf855f9dcc85cd0ad5741e05e7bbfe53d2a69cb4
Sudip <paudelsudip134@gmail.com>
2025-06-09 10:16:37 +0000

    built in modules in py

---
+++ 5_functions_n_modules/4_modules.py
@@ -0,0 +1,13 @@
+import math
+import random
+import datetime
+
+# ✅ math module
+print("Square root of 16:", math.sqrt(16))
+
+# ✅ random module
+print("Random number (1–10):", random.randint(1, 10))
+
+# ✅ datetime module
+now = datetime.datetime.now()
+print("Current date & time:", now.strftime("%Y-%m-%d %H:%M:%S"))
